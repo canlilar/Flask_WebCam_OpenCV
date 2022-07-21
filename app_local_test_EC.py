3c5bcf663c45f1de737a340d4e447a3b586875b3
canlilar <eden.waller@gmail.com>
2022-07-21 19:32:58 +0000

    a few adjustments

--- app_local_test_EC.py
+++ app_local_test_EC.py
@@ -28,29 +28,30 @@ socketio = SocketIO(app,cors_allowed_origins="*")
 camera = Camera(Makeup_artist())
 
 
-# @socketio.on('input image', namespace='/test')
-# def test_message(input):
-#     input = input.split(",")[1]
-#     camera.enqueue_input(input)
-#     image_data = input # Do your magical Image processing here!!
-#     #image_data = image_data.decode("utf-8")
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input = input.split(",")[1]
+    camera.enqueue_input(input)
+    image_data = input # Do your magical Image processing here!!
+    #image_data = image_data.decode("utf-8")
 
-#     img = imread(io.BytesIO(base64.b64decode(image_data)))
-#     cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-#     cv2.imwrite("reconstructed.jpg", cv2_img)
-#     retval, buffer = cv2.imencode('.jpg', cv2_img)
-#     b = base64.b64encode(buffer)
-#     b = b.decode()
-#     image_data = "data:image/jpeg;base64," + b
+    img = imread(io.BytesIO(base64.b64decode(image_data)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    cv2.imwrite("reconstructed.jpg", cv2_img)
+    retval, buffer = cv2.imencode('.jpg', cv2_img)
+    b = base64.b64encode(buffer)
+    b = b.decode()
+    image_data = "data:image/jpeg;base64," + b
 
-#     print("OUTPUT " + image_data)
-#     emit('out-image-event', {'image_data': image_data}, namespace='/test')
-#     #camera.enqueue_input(base64_to_pil_image(input))
+    print("OUTPUT " + image_data)
+    emit('out-image-event', {'image_data': image_data}, namespace='/test')
+    #camera.enqueue_input(base64_to_pil_image(input))
 
 
-# @socketio.on('connect', namespace='/test')
-# def test_connect():
-#     app.logger.info("client connected")
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    app.logger.info("client connected")
+    print("client connected: test")
 
 
 @app.route('/')
@@ -76,6 +77,19 @@ def gen():
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+########## EC additional page for testing #############
+@socketio.on('connect', namespace='/test2')
+def test_connect():
+    app.logger.info("client connected")
+    print("client connected: test2")
+
+@app.route('/visionapi')
+def visionapifunc():
+    """Google Vision Intelligence API test"""
+    # return render_template('index.html')
+    return render_template('full-page-carousel.html')
+######################################################
 
 
 if __name__ == '__main__':
